72806c873201e2ad3e31f5f4ea31648f2e49d4fe
Zverenaklonitel <fruntovoleg@gmail.com>
2025-04-29 17:47:40 +0300

    init

---
+++ DbTest/TestDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DbTest;
+
+public class TestEntity
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+public class TestDbContext : DbContext
+{
+    public TestDbContext(DbContextOptions<TestDbContext> options)
+    {
+        
+    }
+    
+    public DbSet<TestEntity> TestTable { get; set; }
+}
---
+++ DbTest/TestHostedService.cs
@@ -0,0 +1,46 @@
+﻿using Amazon;
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+
+namespace DbTest;
+
+public class TestHostedService : BackgroundService
+{
+    private readonly ILogger<TestHostedService> _logger;
+    private readonly IConfiguration _configuration;
+
+    public TestHostedService(IConfiguration configuration, ILogger<TestHostedService> logger)
+    {
+        _configuration = configuration;
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        string secretName = _configuration.GetSection("RdsSecret").Get<string>()!;
+        string region = "eu-central-1";
+
+        IAmazonSecretsManager client = new AmazonSecretsManagerClient(RegionEndpoint.GetBySystemName(region));
+
+        GetSecretValueRequest request = new GetSecretValueRequest
+        {
+            SecretId = secretName,
+            VersionStage = "AWSCURRENT"
+        };
+
+        GetSecretValueResponse response;
+
+        try
+        {
+            response = await client.GetSecretValueAsync(request, stoppingToken);
+        }
+        catch (Exception e)
+        {
+            throw e;
+        }
+
+        string secret = response.SecretString;
+        
+        _logger.LogInformation(secret);
+    }
+}
